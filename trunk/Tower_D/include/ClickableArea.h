9a4dcfb410bf37887dea3fa5c7872c6e9fe7d424
henninger.joshuam@gmail.com <henninger.joshuam@gmail.com@af8411b0-a30e-eea6-a620-e933f664f179>
2011-11-06 07:59:24 +0000

    ClickableArea.h (used for getting clicks on the grid and the area where towers are purchased)

---
+++ trunk/Tower_D/include/ClickableArea.h
@@ -0,0 +1,22 @@
+#ifndef CLICKABLEAREA_H
+#define CLICKABLEAREA_H
+
+#include <QLabel>
+
+class ClickableArea : public QLabel
+{
+    Q_OBJECT
+
+public:
+    ClickableArea( QWidget * parent = 0 );
+    ~ClickableArea();
+
+signals:
+    void clicked();
+
+protected:
+    void mousePressEvent(QMouseEvent *ev);
+
+};
+
+#endif // CLICKABLEAREA_H
