7770ae5a908281c96271707ea7ff821fb955bdcd
henninger.joshuam@gmail.com <henninger.joshuam@gmail.com@af8411b0-a30e-eea6-a620-e933f664f179>
2011-11-06 07:59:56 +0000

    Model.h

---
+++ trunk/Tower_D/include/Model.h
@@ -0,0 +1,40 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+#include "include/Castle.h"
+#include "include/Monster.h"
+#include "include/Tower.h"
+#include <QWidget>
+#include <QVector>
+
+class Model : public QWidget
+{
+    Q_OBJECT
+
+private:
+    Castle *castle;
+
+    int *path;
+    int pathSize;
+    char *gameTitle;
+    QVector<Monster*> monsters;
+    QVector<Tower*> towers;
+    QVector<int> deadMonsters;
+
+public:
+    explicit Model( QWidget * parent = 0 );
+
+    void addMonster();
+    bool addTower(int grdNmbr);
+    int * getPath();
+    int getPathSize();
+    int getTowersSize();
+    void incrementMonsters();
+    void kill(int twrIndex, int mnstIndex);
+    QVector<int> towersTakeShot();
+
+    Castle * getCastle();
+
+};
+
+#endif // MODEL_H
