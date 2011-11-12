0f953742919ab3b2939ab78e57d7472f59b2f3a9
sharolchand@gmail.com <sharolchand@gmail.com@af8411b0-a30e-eea6-a620-e933f664f179>
2011-11-12 01:24:20 +0000

    added the putTowerT function that returns the selected tower that the player chooses to place on to the grid. The way it differentiates between which tower is selected is based on the grid number. It checks the grid number and returns the tower type accordingly. This function only returns the tower that WILL be placed on to the grid by the controller. It still needs to get implemented by the controller before getting passed to the view and actually being placed on to the grid.

--- trunk/TowerD/debug/moc_View.cpp
+++ trunk/TowerD/debug/moc_View.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'View.h'
 **
-** Created: Tue Nov 8 19:56:44 2011
+** Created: Fri Nov 11 14:51:29 2011
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.7.4)
 **
 ** WARNING! All changes made in this file will be lost!
--- trunk/TowerD/include/TowerArray.h
+++ trunk/TowerD/include/TowerArray.h
@@ -1,8 +1,9 @@
 #ifndef ARRAYTOWERS_H
 #define ARRAYTOWERS_H
-//#include "..\include\Model.h"
+#include "..\include\Model.h"
 #include "..\include\Tower1.h"
 #include "..\include\GraphicsItem.h"
+#include "..\include\Castle.h"
 
 class	ArrayTowers : public GraphicsItem	// ArrayTowers class declaration "Inherited from graphics item"
 {
@@ -28,6 +29,23 @@ class	ArrayTowers : public GraphicsItem	// ArrayTowers class declaration "Inheri
                 Tower1 get_tower3()
                 {
                     return towers[2];
+                }
+                Tower1 putTowerT(int gn)
+                {
+                    int grdno = 0;
+                    grdno = gn;
+                    if (grdno == 205)
+                    {
+                        return towers[0];
+                    }
+                    else if (grdno == 206)
+                    {
+                        return towers[1];
+                    }
+                    else if (grdno == 207)
+                    {
+                        return towers[2];
+                    }
                 }
 
 };
